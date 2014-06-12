14d1fdf49fa20dac1283c965c072a9eb723eb524
Andre Arko & Paul Henry <andre+paul@wanelo.com>
2014-06-12 14:02:32 -0700

    extract standalone warning into boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,2 +1,7 @@
-# Set up gems listed in the Gemfile.
-require 'bundler/setup'
+begin
+  require File.expand_path('../../.bundle/bundler/setup', __FILE__)
+rescue LoadError
+  STDERR.puts 'Run `bin/setup` for faster app booting'
+  require 'rubygems'
+  require 'bundler/setup'
+end
