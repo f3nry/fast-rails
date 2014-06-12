5f4bab0f7cd227fd72e38f6a755a0ff3aa7260ff
Andre Arko & Paul Henry <andre+paul@wanelo.com>
2014-06-12 14:37:27 -0700

    fake out Rubygems so Rails will load

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,16 @@
 begin
   require File.expand_path('../../.bundle/bundler/setup', __FILE__)
+
+  # ActiveRecord requires Gem::LoadError to load
+  module Gem
+    class LoadError < LoadError; end
+    # irb/locale.rb calls this if defined?(Gem)
+    def self.try_activate(*); end
+  end
+
+  # ActiveSupport requires Kernel.gem to load
+  # ActiveRecord requires main.gem to load
+  module Kernel; def gem(*); end; end
 rescue LoadError
   STDERR.puts 'Run `bin/setup` for faster app booting'
   require 'rubygems'
